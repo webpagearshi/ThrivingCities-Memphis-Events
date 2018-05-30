0dc50aec5d437dd755e2823f621e138d0a3a4059
webpagearshi <arshisaleh@gmail.com>
2018-05-30 20:03:33 +0530

    uploading master

---
+++ main.js
@@ -0,0 +1,55 @@
+
+/*navbar scroll*/
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+      if (scroll > 230) {
+        $(".navbar").css("background" , "#1F6ED4");
+      }
+
+      else{
+          $(".navbar").css("background" , "");   
+      }
+  })
+})
+/*cards carousel scroll effect*/
+$("#myCarousel").on("slide.bs.carousel", function(e) {
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 3;
+  var totalItems = $(".carousel-item").length;
+
+  if (idx >= totalItems - (itemsPerSlide - 1)) {
+    var it = itemsPerSlide - (totalItems - idx);
+    for (var i = 0; i < it; i++) {
+      // append slides to end
+      if (e.direction == "left") {
+        $(".carousel-item")
+          .eq(i)
+          .appendTo(".carousel-inner");
+      } else {
+        $(".carousel-item")
+          .eq(0)
+          .appendTo($(this).find(".carousel-inner"));
+      }
+    }
+  }
+});
+/* scroll to the top */
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
